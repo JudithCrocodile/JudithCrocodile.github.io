004930efe58f70dc4116e80924d4330f163b774a
Judith Lee <argentum1218@gmail.com>
2024-09-10 20:25:03 -0400

    feat(tag): change tag style

--- components/Tag.vue
+++ components/Tag.vue
@@ -9,7 +9,7 @@ const { label } = props;
 </script>
 
 <template>
-    <div class="tag inline-block bg-slate-300 text-white px-4 py-1 rounded mt-1 mr-2 font-normal">
+    <div class="tag inline-block bg-slate-300 text-white px-4 pt-1 rounded mt-1 mr-2 font-normal">
         <slot></slot>
     </div>
 </template>
